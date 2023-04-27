6c24b5055d94a31b988ee86e38f11dd63efcfb0c
Rahul Ravindran <43089715+imrhlrvndrn@users.noreply.github.com>
2023-04-27 18:24:09 +0530

    Updated readFile filepath

--- src/pages/api/mail.js
+++ src/pages/api/mail.js
@@ -3,13 +3,18 @@ import * as fs from 'fs';
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
-        let inboxData = await JSON.parse(await fs.readFileSync('json/inbox.json', 'utf-8'));
+        let inboxData = await JSON.parse(
+            await fs.readFileSync(`${process.cwd()}/json/inbox.json`, 'utf-8')
+        );
         await fs.writeFileSync(
             'json/inbox.json',
             JSON.stringify({ ...inboxData, name: 'rahul ravindran' }, null, 4)
         );
 
-        inboxData = await JSON.parse(await fs.readFileSync('json/inbox.json', 'utf-8'));
+        console.log(process.cwd());
+        inboxData = await JSON.parse(
+            await fs.readFileSync(`${process.cwd()}/json/inbox.json`, 'utf-8')
+        );
 
         res.status(200).json({ inbox: inboxData });
     }
